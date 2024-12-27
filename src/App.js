ca99f90bef02f79fc1d3158a84069271a27f114e
AggieBingo <18rking@gmail.com>
2024-12-26 17:12:11 -0700

    first home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> party.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+    <Box sx={{ my: 4 }}>
+      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+       Welcome to Aggie Bingo
+      </Typography>
+      
+    </Box>
+  </Container>
   );
 }
 
