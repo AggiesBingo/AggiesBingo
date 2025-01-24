b54e03474be069d73f4075ff3158aa65f892b419
AggieBingo <18rking@gmail.com>
2025-01-24 10:52:19 -0700

    bug fixes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import bingoGrid from './bingoGrid';
-
-
-
+import { bingoGrid } from './bingoGrid';  // Correct import
 
 function App() {
   return (
     <Container maxWidth="sm">
-    <Box sx={{ my: 4 }}>
-      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-       Welcome to Aggie Bingo
-      </Typography>
-       {bingoGrid}
-    </Box>
-  </Container>
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Welcome to Aggie Bingo
+        </Typography>
+        {bingoGrid()}  {/* Call the function to render */}
+      </Box>
+    </Container>
   );
 }
 
--- src/bingoGrid.js
+++ src/bingoGrid.js
@@ -1,32 +1,28 @@
-import gridItem from "./gridItem";
-
-
 function generateBoard() {
-  const numbers = [];
-  while (numbers.length < 25) {
-    const rand = Math.floor(Math.random() * 75) + 1;
-    if (!numbers.includes(rand)) {
-      numbers.push(rand);
+    const numbers = [];
+    while (numbers.length < 25) {
+      const rand = Math.floor(Math.random() * 75) + 1;
+      if (!numbers.includes(rand)) {
+        numbers.push(rand);
+      }
     }
+    return numbers;
   }
-  return numbers;
-}
-
-function gridItem(index) {
-    return(
-        <div>
-            {index}
-        </div>
+  
+  function gridItem(num, index) {  // Pass 'index' for the 'key'
+    return (
+      <div key={index}>
+        {num}
+      </div>
     );
-    }
-
-export function bingoGrid(){
-    const boardNumbers = generateBoard;
-return(
-    <div>
-        {boardNumbers.map((num, index) => (
-            gridItem(index)
-        ))}
-
-    </div>
-);}
+  }
+  
+  export function bingoGrid() {
+    const boardNumbers = generateBoard();
+    return (
+      <div>
+        {boardNumbers.map((num, index) => gridItem(num, index))}
+      </div>
+    );
+  }
+  
